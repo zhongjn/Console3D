d48b10e45ffa8247e6206918e85cd24ad1b8d06c
zhongjn <2389206378@qq.com>
2018-05-26 22:05:46 +0800

    弃用老文件

--- Depreciated/mesh.cpp
+++
@@ -1,9 +0,0 @@
-#include "my3d.h"
-
-console3d::my3d::Mesh::Mesh(std::vector<Vertex> vertexes, std::vector<Triangle> triangles)
-	: vertexes(vertexes), triangles(triangles)
-{
-
-}
-
-console3d::my3d::Mesh::Mesh() {}
